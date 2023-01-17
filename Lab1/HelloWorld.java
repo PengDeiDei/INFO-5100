7297b70d04e9ccb9ba8a6df8b216a70b3d4fc3c6
hap045 <Hap045@ucsd.edu>
2023-01-17 09:15:57 -0800

    Update HelloWorld.java

--- Lab1/HelloWorld.java
+++ Lab1/HelloWorld.java
@@ -1,13 +1,16 @@
 public class HelloWorld{
     private String greeting = "Hello, World!";
-    public void greet(String msg){
-        greeting = msg;
+    public void greet(){
         System.out.println(greeting);
     }
 
+    public void getGreet(String msg){
+        this.greeting = msg;
+    }
     public static void main(String args[]){
         HelloWorld helloworld;
         helloworld = new HelloWorld();
-        helloworld.greet("Hello, there!") ;
+        helloworld.getGreet("Hello, northeastern!");
+        helloworld.greet() ;
     }
 }
