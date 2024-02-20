584a4faa8894a9415fd778fa98d88560e5fb229c
hap045 <Hap045@ucsd.edu>
2024-02-20 12:36:25 -0800

    Add fun with GUI

---
+++ assignment-6-fun-with-gui/Lab6_GUI/src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * author: Haonan Peng
+ *
+ * This is the tester main class for the GUI class in lab 6
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Lab2GUI();
+    }
+}
